5fe44e8141778519a0efa4d056d4bb2d97d76635
Anton Chaiko <a.chaiko.vironit@vironit.ru>
2021-07-28 13:32:40 +0300

    feat: implement google sign in

--- app/src/main/java/com/example/cryptocurrency/ui/screens/loginfragment/LoginFragment.kt
+++ app/src/main/java/com/example/cryptocurrency/ui/screens/loginfragment/LoginFragment.kt
@@ -1,31 +1,93 @@
 package com.example.cryptocurrency.ui.screens.loginfragment
 
-import androidx.lifecycle.ViewModelProvider
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.NonNull
+import androidx.fragment.app.Fragment
 import com.example.cryptocurrency.R
 import com.example.cryptocurrency.databinding.LoginFragmentBinding
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+const val RC_SIGN_IN = 1
 
 class LoginFragment : Fragment() {
 
     private lateinit var viewModel: LoginViewModel
     private lateinit var binding: LoginFragmentBinding
+    private lateinit var googleSignInClient: GoogleSignInClient
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         binding = LoginFragmentBinding.inflate(inflater)
+        auth = Firebase.auth
+
+        val gso = GoogleSignInOptions
+            .Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(requireActivity(), gso)
+
+        binding.signInButton.setOnClickListener {
+            signIn()
+        }
+        binding.logOutButton.setOnClickListener {
+            googleSignInClient.signOut()
+            auth.signOut()
+        }
         return binding.root
     }
 
+    private fun signIn() {
+        startActivityForResult(googleSignInClient.signInIntent, RC_SIGN_IN)
+    }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(LoginViewModel::class.java)
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                val account = task.getResult(ApiException::class.java)!!
+                firebaseAuthWithGoogle(account.idToken!!)
+            } catch (e: ApiException) {
+                Log.e("asd", "Google sign in failed", e)
+            }
+        }
+    }
+    private fun firebaseAuthWithGoogle(idToken: String) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(requireActivity()) { task ->
+                if (task.isSuccessful) {
+                    Log.d("asd", "signInWithCredential:success")
+                    val user = auth.currentUser
+
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w("asd", "signInWithCredential:failure", task.exception)
+                }
+            }
     }
 
 }
+
+
+
--- app/src/main/java/com/example/cryptocurrency/ui/screens/loginfragment/LoginViewModel.kt
+++ app/src/main/java/com/example/cryptocurrency/ui/screens/loginfragment/LoginViewModel.kt
@@ -1,7 +1,8 @@
 package com.example.cryptocurrency.ui.screens.loginfragment
 
+import androidx.core.app.ActivityCompat.startActivityForResult
 import androidx.lifecycle.ViewModel
 
 class LoginViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
 }
